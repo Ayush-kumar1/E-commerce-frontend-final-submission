b47a1d895deae7f5b79b98c340a299b5d1da2e72
Ayush <ayush.ak5555@gmail.com>
2021-05-07 16:22:54 +0530

    files updated

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,7 +9,7 @@ import {Link} from "react-router-dom"
 
 const Header = () => {
     return (
-        <div className="header">
+        <div className="nav">
             <div className="logo_position"> 
 
             <Link to="/">
